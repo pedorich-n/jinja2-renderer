8edee0936b72f4dcbc269958eedc36f8d420af24
Nikita Pedorich <pedorich.n@gmail.com>
2024-08-10 22:53:59 +0900

    Add links

--- flake-parts/modules/lib.nix
+++ flake-parts/modules/lib.nix
@@ -1,3 +1,5 @@
+# Based on https://github.com/hercules-ci/flake-parts/blob/8471fe90ad337a8074e957b69ca4d0089218391d/modules/checks.nix
+# And https://github.com/hercules-ci/flake-parts/issues/220#issuecomment-2053718001
 { lib, flake-parts-lib, ... }:
 let
   inherit (lib)
